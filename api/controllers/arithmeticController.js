ab29f19149f4567a5be004500be161033fff3d5a
HNeukermans <hannes_neukermans@hotmail.com>
2019-06-03 12:33:17 +0200

    Update arithmeticController.js

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -13,7 +13,7 @@ exports.calculate = function(req, res) {
   var operations = {
     // Using `+a + +b` will coerce a and b to numbers.  Without that, this will perform string concatenation.
     // (ie, `42 + 0` => `420`)
-    'add':      function(a,b) { return +a + +b },
+    'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
